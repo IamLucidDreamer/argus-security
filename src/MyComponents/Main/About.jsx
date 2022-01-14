a8da841599986b5c2bf730e97ed3dfec0887dc71
Manas Shukla <shuklamanasofficial@gmail.com>
2022-01-15 01:39:38 +0530

    UI Changes

--- src/MyComponents/Main/About.jsx
+++ src/MyComponents/Main/About.jsx
@@ -13,7 +13,7 @@ class About extends Component {
         <div className="text-gray-600 body-font bg-no-repeat bg-cover bg-center bg-aboutbg">
           <div className="container mx-auto flex px-5 py-20 md:py-40 items-center justify-center flex-col">
             <div className="text-center lg:w-2/3 w-full">
-              <h1 className="title-font sm:text-6xl text-3xl mb-4 font-bold font-for-para text-white">
+              <h1 className="title-font sm:text-6xl text-3xl mb-4 font-bold font-for-para text-white ">
                 ABOUT US
               </h1>
             </div>
